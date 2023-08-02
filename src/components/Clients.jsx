b8bf30f972c7340d50930ad6637c6575bf3d9ade
Krish1116 <chotaliyakrishana1116@gmail.com>
2023-08-02 12:28:53 +0530

    Hoobank project1

---
+++ src/components/Clients.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "../stlye";
+import { clients } from "../constants";
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((ele) => (
+        <div
+          key={ele.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={ele.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default Clients;
